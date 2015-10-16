0d3288e0207a76ab1a7e3ba80d847914833fe434
Alex Castillo <alex@castillo.io>
2015-10-16 14:53:17 -0400

    Initial demo

---
+++ demo/app.js
@@ -0,0 +1,18 @@
+(function() {
+
+    angular
+        .module('breakpointDemo', ['ghsBreakpoint'])
+        .config(breakpointConfig);
+
+    function breakpointConfig ($breakpointProvider) {
+
+        $breakpointProvider.set({
+            xs: '(max-width: 480px)',
+            sm: '(min-width: 481px) and (max-width: 768px)',
+            md: '(min-width: 769px) and (max-width: 991px)',
+            lg: '(min-width: 992px)'
+        });
+
+    }
+
+})();
